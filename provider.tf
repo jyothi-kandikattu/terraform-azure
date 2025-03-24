30b7e2c30a9f190b64d8bf825e499afcc6fa5ee1
jyothi-kandikattu <bestofjyothi@gmail.com>
2025-03-23 20:14:15 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.18.0"
+    }
+    
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+ 
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  features {}
+}
