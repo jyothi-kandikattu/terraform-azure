622af0a81fa4280436e5c8c60f60307a703945e7
jyothi-kandikattu <bestofjyothi@gmail.com>
2025-03-23 20:02:01 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" {
+  type = "8facf10a-9b59-4bf3-9a50-c3f6a55bfb44"
+}
+variable "tenant_id" {
+  type = "b96f9db9-e633-414e-8274-5182a2e9154b"
+}
+variable "client_id" {
+  type = "1a6ca4be-cf73-49ca-822d-8f71b3c0277d"
+}
+variable "client_secret" {
+  type = "d5q8Q~4fcmrWxyCVlnEFjah8VteISpf7os6NnafC"
+}
